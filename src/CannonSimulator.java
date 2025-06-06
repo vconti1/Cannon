1dad8272b3ebb42c3e2a24ff31903b6f09e0216f
vconti1 <vincentconti393@gmail.com>
2025-06-06 15:37:35 -0400

    Create simulator window and set dimensions

--- src/CannonSimulator.java
+++ src/CannonSimulator.java
@@ -1,4 +1,6 @@
 import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -9,6 +11,17 @@ import javax.swing.JPanel;
 
 public class CannonSimulator extends JPanel implements ActionListener, KeyListener {
 
+	final int boardWidth = 1080;
+	final int boardHeight = 720;
+	
+	CannonSimulator(){
+		setPreferredSize(new Dimension(boardWidth,boardHeight));
+		setBackground(Color.black);
+		setFocusable(true);
+		addKeyListener(this);
+	}
+	
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
+import javax.swing.JFrame;
+import java.awt.Graphics;
 
 public class Main {
+	public static void main(String[] args) {
+		int boardWidth = 1080;
+		int boardHeight = 720;
+		
+		JFrame frame = new JFrame("Cannon Simulator");
+		
 
+		frame.setSize(boardWidth, boardHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		CannonSimulator buildingBlocks = new CannonSimulator();
+		frame.add(buildingBlocks);
+		frame.pack();
+		buildingBlocks.requestFocus();
+		frame.setVisible(true);
+	}
 }
